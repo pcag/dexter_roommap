179ac1ca6596cd825dce4713dcaff06cb752c224
Florian Röser <mail@roeser-rombach.de>
2016-12-13 20:29:35 +0100

    add controllers from findhole project

---
+++ roommap/main.py
@@ -0,0 +1,25 @@
+
+from controller import Controller
+
+
+def main():
+    print ""
+    print "##########################"
+    print "####  starting ###########"
+    print "##########################"
+    print ""
+
+    ctrl = Controller()
+    ctrl.run()
+
+    print ""
+    print "##########################"
+    print "####  stopping ###########"
+    print "##########################"
+    print ""
+
+    pass
+
+
+if __name__ == "__main__":
+    main()
