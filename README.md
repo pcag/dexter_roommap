cb5e947ed5f0d8bc599474f91eb09a77d415ae74
Florian Röser <mail@roeser-rombach.de>
2017-02-23 15:28:12 +0100

    update readme und controller

--- README.md
+++ README.md
@@ -6,3 +6,71 @@ Using a dexter GoPiGo to create a map of a room
 
 * install ```pip``` (https://pip.pypa.io/en/stable/installing/)
 * install gopigo library: ```sudo pip install gopigo```
+
+
+# Setup Dexter
+
+Der Ultraschallsensor muss an den A1 Port angeschlossen werden.  
+Der Servo wird vorne in der Mitte an den 3-PIN port angeschlossen.
+
+# Direkte Steuerung
+
+gehe in das projekt
+```python
+cd ~/Desktop/roommap
+```
+
+gehe in die python console und lade den controller
+```python
+python
+>>> from controller import *
+```
+
+## Fahren
+
+forwärts (20 cm)
+```python
+fwd_cm(20)
+```
+
+rückwärts (20cm)
+```python
+bwd_cm(20)
+```
+
+stop
+```python
+stop()
+```
+
+links drehen (grad)
+```python
+left_deg(45)
+```
+
+rechts drehen (grad)
+```python
+right_deg(45)
+```
+
+## Servo steuerung
+
+Der servo hat eine Bandbreite von 0 - 180 grad  
+Mittelstellung ist 90 grad
+
+```python
+servo(90)
+```
+
+## Ultraschall Sensor
+
+Der Ultraschallsensor gibt die Maße in cm an
+
+Wenn der Ultraschallsensor an Port A1 angeschlossen ist, so ist der "Pin" **15**
+
+Port: A1 = pin: 15
+
+ultraschall-sensor befehl:  `us_dist(<pin>) `
+```python
+us_dist(15)
+```
--- roommap/controller.py
+++ roommap/controller.py
@@ -29,6 +29,72 @@ CHASS_WID = 13.5  # Chassis is ~13.5 cm wide.
 en_debug = 1
 
 
+def cm2pulse(dist):
+    """
+    Calculate the number of pulses to move the chassis dist cm.
+    pulses = dist * [pulses/revolution]/[dist/revolution]
+    """
+    wheel_circ = 2 * math.pi * WHEEL_RAD  # [cm/rev] cm traveled per revolution of wheel
+    revs = dist / wheel_circ
+    PPR = 18  # [p/rev] encoder Pulses Per wheel Revolution
+    pulses = PPR * revs  # [p] encoder pulses required to move dist cm.
+    if en_debug:
+        print 'WHEEL_RAD', WHEEL_RAD
+        print 'revs', revs
+        print 'pulses', pulses
+    return pulses
+
+
+def left_deg(deg=None):
+    """
+    Turn chassis left by a specified number of degrees.
+    DPR is the #deg/pulse (Deg:Pulse ratio)
+    This function sets the encoder to the correct number
+     of pulses and then invokes left().
+    """
+    if deg is not None:
+        pulse = int(deg / DPR)
+        enc_tgt(0, 1, pulse)
+    left()
+
+
+def right_deg(deg=None):
+    """
+    Turn chassis right by a specified number of degrees.
+    DPR is the #deg/pulse (Deg:Pulse ratio)
+    This function sets the encoder to the correct number
+     of pulses and then invokes right().
+    """
+    if deg is not None:
+        pulse = int(deg / DPR)
+        enc_tgt(0, 1, pulse)
+    right()
+
+
+def fwd_cm(dist=None):
+    """
+    Move chassis fwd by a specified number of cm.
+    This function sets the encoder to the correct number
+     of pulses and then invokes fwd().
+    """
+    if dist is not None:
+        pulse = int(cm2pulse(dist))
+        enc_tgt(1, 1, pulse)
+    fwd()
+
+
+def bwd_cm(dist=None):
+    """
+    Move chassis bwd by a specified number of cm.
+    This function sets the encoder to the correct number
+     of pulses and then invokes bwd().
+    """
+    if dist is not None:
+        pulse = int(cm2pulse(dist))
+        enc_tgt(1, 1, pulse)
+    bwd()
+
+
 class Controller(object):
     """
     Die Controller Klasse ist für die eigentliche Kontrolle
@@ -78,69 +144,3 @@ class Controller(object):
         stop()
         print "Found obstacle"
         return
-
-    @staticmethod
-    def left_deg(deg=None):
-        """
-        Turn chassis left by a specified number of degrees.
-        DPR is the #deg/pulse (Deg:Pulse ratio)
-        This function sets the encoder to the correct number
-         of pulses and then invokes left().
-        """
-        if deg is not None:
-            pulse = int(deg / DPR)
-            enc_tgt(0, 1, pulse)
-        left()
-
-    @staticmethod
-    def right_deg(deg=None):
-        """
-        Turn chassis right by a specified number of degrees.
-        DPR is the #deg/pulse (Deg:Pulse ratio)
-        This function sets the encoder to the correct number
-         of pulses and then invokes right().
-        """
-        if deg is not None:
-            pulse = int(deg / DPR)
-            enc_tgt(0, 1, pulse)
-        right()
-
-    @staticmethod
-    def fwd_cm(dist=None):
-        """
-        Move chassis fwd by a specified number of cm.
-        This function sets the encoder to the correct number
-         of pulses and then invokes fwd().
-        """
-        if dist is not None:
-            pulse = int(cm2pulse(dist))
-            enc_tgt(1, 1, pulse)
-        fwd()
-
-    @staticmethod
-    def bwd_cm(dist=None):
-        """
-        Move chassis bwd by a specified number of cm.
-        This function sets the encoder to the correct number
-         of pulses and then invokes bwd().
-        """
-        if dist is not None:
-            pulse = int(cm2pulse(dist))
-            enc_tgt(1, 1, pulse)
-        bwd()
-
-    @staticmethod
-    def cm2pulse(dist):
-        """
-        Calculate the number of pulses to move the chassis dist cm.
-        pulses = dist * [pulses/revolution]/[dist/revolution]
-        """
-        wheel_circ = 2 * math.pi * WHEEL_RAD  # [cm/rev] cm traveled per revolution of wheel
-        revs = dist / wheel_circ
-        PPR = 18  # [p/rev] encoder Pulses Per wheel Revolution
-        pulses = PPR * revs  # [p] encoder pulses required to move dist cm.
-        if en_debug:
-            print 'WHEEL_RAD', WHEEL_RAD
-            print 'revs', revs
-            print 'pulses', pulses
-        return pulses
