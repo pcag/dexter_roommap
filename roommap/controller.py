e8d50ef2e52f66b39c8b9451b59f4a1a90e522c5
Florian Röser <mail@roeser-rombach.de>
2017-03-09 16:24:51 +0100

    update controller routinge

--- roommap/controller.py
+++ roommap/controller.py
@@ -3,6 +3,8 @@
 
 from gopigo import *
 import time
+import math
+import random
 
 # constants
 
@@ -11,7 +13,7 @@ import time
 FAILSAFE_STOP = 20
 
 # Entfernung in cm wenn der GoPiGo anhalten soll
-STOP_DISTANCE = 20
+STOP_DISTANCE = 30
 
 # Zeit, die zwischen den Iterationen liegen soll (in sekunden)
 SLEEP_TIME = .200
@@ -25,6 +27,10 @@ INFINITY = 200
 DPR = 360.0 / 64
 WHEEL_RAD = 3.25  # Wheels are ~6.5 cm diameter.
 CHASS_WID = 13.5  # Chassis is ~13.5 cm wide.
+
+SAMPLES = 4  # Number of sample readings to take for each reading.
+DELAY = .02
+INF = 200
 
 en_debug = 1
 
@@ -95,6 +101,74 @@ def bwd_cm(dist=None):
     bwd()
 
 
+def scan_room():
+    '''
+    Start at 0 and move to 180 in increments.
+    Angle required to fit chass @20cm away is:
+        degrees(atan(CHASS_WID/20))
+    Increments angles should be 1/2 of that.
+    Looking for 3 consecutive readings of inf.
+    3 misses won't guarantee a big enough hole
+     because not every obstacle will be 20cm away,
+     but it is a good place to start, and more
+     importantly, gives us edges to use to
+     measure.
+
+    Return list of (angle,dist).
+    '''
+    ret = []
+    #inc = int(math.degrees(math.atan(CHASS_WID / 20)))
+    inc = 10
+    print "Scanning room in {} degree increments".format(inc)
+    for ang in range(0, 181, inc):
+        print "  Setting angle to {} ... ".format(ang),
+        ## resetting ang because I've seen issues with 0 and 180
+        if ang == 0: ang = 1
+        if ang == 180: ang = 179
+        servo(ang)
+        buf = []
+        for i in range(SAMPLES):
+            dist = us_dist(15)
+            print dist,
+            if dist < INF and dist >= 0:
+                buf.append(dist)
+            else:
+                buf.append(INF)
+        print
+        ave = math.fsum(buf) / len(buf)
+        print "  dist={}".format(ave)
+        ret.append((ang, ave))
+        ## Still having issues with inconsistent readings.
+        ## e.g.
+        ##  Setting angle to   0 ...    18   19 218 49
+        ##  Setting angle to 170 ...  1000 1000  45 46
+        time.sleep(DELAY)
+    ## Reset servo to face front
+    servo(90)
+    return ret
+
+
+def obstacle_in_front(distances):
+    if distances[7][1] < STOP_DISTANCE or distances[8][1] < STOP_DISTANCE or distances[9][1] < STOP_DISTANCE or distances[10][1] < STOP_DISTANCE or distances[11][1] < STOP_DISTANCE:
+        return True
+    else:
+        return False
+
+
+def obstacle_right(distances):
+    if distances[0][1] < STOP_DISTANCE or distances[1][1] < STOP_DISTANCE or distances[2][1] < STOP_DISTANCE or distances[3][1] < STOP_DISTANCE or distances[4][1] < STOP_DISTANCE or distances[5][1] < STOP_DISTANCE:
+        return True
+    else:
+        return False
+
+
+def obstacle_left(distances):
+    if distances[13][1] < STOP_DISTANCE or distances[14][1] < STOP_DISTANCE or distances[15][1] < STOP_DISTANCE or distances[16][1] < STOP_DISTANCE or distances[17][1] < STOP_DISTANCE or distances[18][1] < STOP_DISTANCE:
+        return True
+    else:
+        return False
+
+
 class Controller(object):
     """
     Die Controller Klasse ist für die eigentliche Kontrolle
@@ -110,6 +184,8 @@ class Controller(object):
         self.goMax = FAILSAFE_STOP
         self.goCounter = 0
         self.go = True
+        ## reduzierte geschwindigkeit, damit die raeder nicht durch drehen
+        set_speed(80)
 
     def run(self):
         print "run Controller()"
@@ -118,7 +194,34 @@ class Controller(object):
             # print "iteration %d" % self.goCounter
 
             # fahre vorwärts, solange wir kein Hinderniss finden
-            self.move(STOP_DISTANCE)
+            # self.move(STOP_DISTANCE)
+
+            # check distance
+            scan_results = scan_room()
+
+            # wenn geradeaus (80, 90, 100 Grad) gemessene dist < 30cm
+            # dann drehe X
+            if obstacle_in_front(scan_results):
+                if obstacle_left(scan_results) and not obstacle_right(scan_results):
+                    right_deg(70)
+                elif not obstacle_left(scan_results) and obstacle_right(scan_results):
+                    left_deg(70)
+                elif obstacle_left(scan_results) and obstacle_right(scan_results):
+                    left_deg(180)
+                else:
+                    # zufaellig links oder rechts
+                    if random.randint(0, 1):
+                        left_deg(70)
+                    else:
+                        right_deg(70)
+            else:
+                # ansonsten fahr X geradeaus
+                if obstacle_left(scan_results):
+                    right_deg(25)
+                elif obstacle_right(scan_results):
+                    left_deg(25)
+                fwd(20)
+            
 
             # safety check %
             # stop after max-counter is reached
